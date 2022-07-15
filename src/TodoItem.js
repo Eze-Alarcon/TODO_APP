876d0729595e139547819d80982ff987e0892651
ezequiel <ps3ezequiel@gmail.com>
2022-07-15 18:23:55 +0200

    completed todos css ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,14 +5,14 @@ const TodoItem = (props) =>{
                 {
                     (props.completed) && (
                         <>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
                         </>
                     )
                 }
             </i>
             <p className="text">{props.text}</p>
-            <i class="icon-delete">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+            <i className="icon-delete">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
             </i>
         </li>
     );
